759305ea6ceb356433fc2a4ee8b4af617933df0f
Jan Hangebrauck <jan@dao.cg>
2025-05-21 15:09:34 +0200

    add tree generation

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
@@ -23,6 +24,9 @@ export function Welcome() {
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
+            </p>
+            <p>
+              <NavLink to="/maketree">Maketree</NavLink>
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
