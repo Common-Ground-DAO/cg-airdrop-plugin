70748f791ece80b7eb227e3bb002ae73cf0045bd
Jan Hangebrauck <jan@dao.cg>
2025-09-01 10:26:28 +0200

    update home page

--- app/components/home/home.tsx
+++ app/components/home/home.tsx
@@ -9,24 +9,22 @@ export default function Home() {
 
     return (
         <div className="flex flex-col gap-4 flex-1 h-full max-h-full overflow-hidden">
-            <h1 className="text-xl font-bold p-4 pb-0">CG Airdrop & Vesting Plugin</h1>
-            <div className="alert alert-warning shadow-lg mt-4 mx-4">
-                <div>
-                    <span className="font-bold">Warning:</span> The smart contracts used by this plugin have <b>not</b> been reviewed yet. Use this plugin for <b>testing purposes only</b>.
-                </div>
-            </div>
-            <div className="p-4 max-w-2xl">
+            <h1 className="text-xl font-bold p-4 pb-0">Common Ground Airdrops & Vestings</h1>
+            <div className="p-4 max-w-full">
                 <p className="mb-2">
-                    This plugin allows admins to manage token airdrops and vestings for this community. Users can view and claim the airdrops and release the vestings.
+                    Here you can claim your airdrops and vestings.
                 </p>
-                <ul className="list-disc list-inside mb-2">
-                    <li>Connect your wallet to see your airdrops and vestings.</li>
-                    <li>Community admins can create new airdrops and vestings.</li>
-                    <li>Upload a CSV file with recipient addresses and token amounts to create an airdrop.</li>
-                    <li>Set up vestings to release tokens over time to a beneficiary.</li>
-                </ul>
+                <p className="mb-2">
+                    <ul className="list-disc list-inside my-4">
+                        <li>If you open an airdrop, connect your beneficiary wallet to see your own airdrop on top</li>
+                        <li>If you open a vesting, connect your beneficiary wallet to be able to claim your tokens</li>
+                    </ul>
+                </p>
                 <p>
-                    This plugin and the smart contracts are available on <a href="https://github.com/common-ground-dao/cg-airdrop-plugin" onClick={(ev) => navigateLink(ev.currentTarget)} className="link link-primary" target="_blank" rel="noopener noreferrer">GitHub</a>.
+                    The smart contracts are available on <a href="https://github.com/common-ground-dao/cg-airdrop-plugin" onClick={(ev) => navigateLink(ev.currentTarget)} className="link link-primary" target="_blank" rel="noopener noreferrer">GitHub</a>.
+                </p>
+                <p>
+                    If you need any help, please contact us on <a href="https://app.cg/c/commonground/channel/help/" onClick={(ev) => navigateLink(ev.currentTarget)} className="link link-primary" target="_blank" rel="noopener noreferrer">our help channel</a>.
                 </p>
             </div>
         </div>
